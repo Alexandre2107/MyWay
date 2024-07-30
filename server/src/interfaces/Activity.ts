f87fa7932823f19ffbe27043a6e30381e40206a9
FilipeSS01 <filipess671@gmail.com>
2024-07-30 17:08:00 -0300

    feature: Ajustando interface com o modelo de banco de dados

--- server/src/interfaces/Activity.ts
+++ server/src/interfaces/Activity.ts
@@ -1,21 +1,26 @@
+import { ActivitySchedule } from "./ActivitySchedule"
 import { Routine } from "./Routine"
 
 export interface Activity {
   activity_id: number
   routine: Routine
-  type: string
+  title: string
   description?: string
+  activity_type: "scheduled" | "task"
+  schedules: ActivitySchedule[]
 }
 
 export interface CreateActivityInput {
   routine_id: number
-  type: string
+  title: string
   description?: string
+  activity_type: "scheduled" | "task"
 }
 
 export interface UpdateActivityInput {
   activity_id: number
   routine_id?: number
-  type?: string
+  title?: string
   description?: string
+  activity_type?: "scheduled" | "task"
 }
--- server/src/interfaces/ActivitySchedule.ts
+++ server/src/interfaces/ActivitySchedule.ts
@@ -3,22 +3,25 @@ import { Activity } from "./Activity"
 export interface ActivitySchedule {
   activity_schedule_id: number
   activity: Activity
-  start_time: Date
-  end_time: Date
-  day_of_week: "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun"
+  has_time: boolean
+  start_time?: Date
+  end_time?: Date
+  day_of_week: "Sun" | "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" 
 }
 
 export interface CreateActivityScheduleInput {
   activity_id: number
-  start_time: Date
-  end_time: Date
-  day_of_week: "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun"
+  has_time: boolean
+  start_time?: Date
+  end_time?: Date
+  day_of_week: "Sun" | "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat"
 }
 
 export interface UpdateActivityScheduleInput {
   activity_schedule_id: number
   activity_id?: number
+  has_time?: boolean
   start_time?: Date
   end_time?: Date
-  day_of_week?: "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat" | "Sun"
+  day_of_week?: "Sun" | "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat"
 }
