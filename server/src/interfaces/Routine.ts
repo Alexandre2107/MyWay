1b766baf7bb7944bd10e7d6a42fb3070a7f5d543
FilipeSS01 <filipess671@gmail.com>
2024-08-08 23:36:40 -0300

    feature: Routine controller

--- server/src/interfaces/Routine.ts
+++ server/src/interfaces/Routine.ts
@@ -2,21 +2,21 @@ import { User } from "./User"
 
 export interface Routine {
   routine_id: number
-  student: User
-  supervisor: User
+  user: User
+  guardian: User
   creation_date: Date
-  description: string
+  description: string | null
 }
 
 export interface CreateRoutineInput {
-  student_id: number
-  supervisor_id: number
+  user_id: number
+  guardian_id: number
   description: string
 }
 
 export interface UpdateRoutineInput {
   routine_id: number
-  student_id?: number
-  supervisor_id?: number
+  user_id?: number
+  guardian_id?: number
   description?: string
 }
