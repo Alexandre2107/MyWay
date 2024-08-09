9604cb9f8a9003c30b71124d9bd487efc1217969
FilipeSS01 <filipess671@gmail.com>
2024-08-08 23:32:03 -0300

    refactor: interface de usuario refatorada

--- server/src/interfaces/User.ts
+++ server/src/interfaces/User.ts
@@ -1,12 +1,12 @@
-  export interface User {
-    user_id: number
-    full_name: string
-    document: string
-    email: string
-    password: string
-    type_of_user: string
-    has_full_permission: boolean
-  }
+export interface User {
+  user_id: number
+  full_name: string
+  document: string
+  email: string
+  password: string
+  type_of_user: string
+  has_full_permission: boolean
+}
 
 export interface CreateUserInput {
   full_name: string
