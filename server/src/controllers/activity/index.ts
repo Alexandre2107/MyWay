f9fa9096c2c1ff5b7a6f7ef87c348d6a6fb9f3b6
FilipeSS01 <filipess671@gmail.com>
2024-08-12 20:53:01 -0300

    feature: updating activity controller

--- server/src/controllers/activity/index.ts
+++ server/src/controllers/activity/index.ts
@@ -6,6 +6,7 @@ import {
   UpdateActivityInput,
 } from "../../interfaces/Activity"
 
+// Get all activity
 export const getActivity = async (
   req: Request,
   res: Response
@@ -13,6 +14,56 @@ export const getActivity = async (
   try {
     const activities: Activity[] = await prisma.activity.findMany({})
     res.status(200).json(activities)
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+}
+
+// Create new activity
+export const createActivity = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const data: CreateActivityInput = req.body
+  try {
+    const activities: Activity = await prisma.activity.create({
+      data: data,
+    })
+    res.status(200).json(activities)
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+}
+
+// Update activity
+export const updateActivity = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { activity_id } = req.params
+  const data: UpdateActivityInput = req.body
+  try {
+    const activities: Activity = await prisma.activity.update({
+      where: { activity_id: Number(activity_id) },
+      data: data,
+    })
+    res.status(200).json(activities)
+  } catch (error) {
+    res.status(500).json({ error: error })
+  }
+}
+
+// Delete activity
+export const deleteActivity = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { activity_id } = req.params
+  try {
+    await prisma.activity.delete({
+      where: { activity_id: Number(activity_id) },
+    })
+    res.status(200).json()
   } catch (error) {
     res.status(500).json({ error: error })
   }
