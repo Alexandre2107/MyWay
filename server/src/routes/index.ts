5bcd26471b36fcee74661f47add1932de97384bb
FilipeSS01 <filipess671@gmail.com>
2024-08-27 11:23:52 -0300

    feature: controller, routes and interface of dayofweek created

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express"
 import userRouter from "./user"
 import routineRouter from "./routine"
 import activityRouter from "./activity"
+import dayOfWeekRouter from "./dayOfWeek"
 
 const router = express.Router()
 
 router.use("/user", userRouter)
 router.use("/routine", routineRouter)
 router.use("/activity", activityRouter)
+router.use("/dayofweek", dayOfWeekRouter)
 
 export default router
